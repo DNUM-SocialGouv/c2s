0b6c1292c4b83896792ced12f7649568aaf096eb
Tlili <sami.tlili@sg.social.gouv.fr>
2024-04-29 15:41:02 +0200

    feat: setup error component

---
+++ src/components/common/error/Error.tsx
@@ -0,0 +1,7 @@
+export const Error = () => {
+  return (
+    <div className="fr-alert fr-alert--error fr-alert--sm bg-white">
+      <p>Erreur : Veuillez réassyer ultérieurement</p>
+    </div>
+  );
+};

531993926ec9e087e0f26eb4f3d0be6be4e8eae5
Tlili <sami.tlili@sg.social.gouv.fr>
2024-05-02 14:39:42 +0200

    refactor: use aria hidden true for svg

--- src/components/common/svg/Hospital.tsx
+++ src/components/common/svg/Hospital.tsx
@@ -6,6 +6,7 @@ export const Hospital = () => {
       viewBox="0 0 65 64"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
     >
       <path
         fillRule="evenodd"
--- src/components/common/svg/Seperator.tsx
+++ src/components/common/svg/Seperator.tsx
@@ -1,7 +1,12 @@
 export const Separator = () => {
   return (
     <div className="oc__separator--padding">
-      <svg height="1" width="1248" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        height="1"
+        width="1248"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+      >
         <line x1="0" y1="0" x2="1248" y2="0" stroke="#DDDDDD" />
       </svg>
     </div>

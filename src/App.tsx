1f8a39fb4faa3f6917f52ee33e2152bbad6ef7aa
nkhayech <nesrine.khayeche@capgemini.com>
2024-05-06 14:46:38 +0200

    C2S-153 - Espace connecté Partenaires - Mes établissements

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import RequireAuth from "@/keycloak/RequireAuth.tsx";
 import { Header } from "@/components/header/Header.tsx";
 import { Footer } from "@/components/footer/Footer.tsx";
 import { useKeycloak } from "@react-keycloak/web";
-import React from 'react';
 const App = () => {
   const logoutOptions = {};
   const { keycloak } = useKeycloak();

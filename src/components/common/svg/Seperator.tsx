eba8ef26456d4a77448ccbd0a2f6bc92804d1dae
Tlili <sami.tlili@sg.social.gouv.fr>
2024-05-07 16:31:58 +0200

    fix: fixing separator width for mobile

--- src/components/common/svg/Seperator.tsx
+++ src/components/common/svg/Seperator.tsx
@@ -3,11 +3,11 @@ export const Separator = () => {
     <div className="oc__separator--padding">
       <svg
         height="1"
-        width="1248"
+        width="100%"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
       >
-        <line x1="0" y1="0" x2="1248" y2="0" stroke="#DDDDDD" />
+        <line x1="0" y1="0" x2="100%" y2="0" stroke="#DDDDDD" />
       </svg>
     </div>
   );

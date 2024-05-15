e84979f26bd66c8796585337b13c429543ed7985
Tlili <sami.tlili@sg.social.gouv.fr>
2024-05-15 17:33:22 +0200

    feat: adding char count for text editor

--- src/components/common/textEditor/TextEditor.tsx
+++ src/components/common/textEditor/TextEditor.tsx
@@ -1,12 +1,32 @@
 import { Editor } from '@tinymce/tinymce-react';
+import { useState } from 'react';
+import SubmitButton from '../submitButton/SubmitButton';
 
 export const TextEditor = () => {
+  const [isDisabled] = useState<boolean>(false);
+  const [value, setValue] = useState(`<p>Le mot de l'équipe C2S</p>`);
+  const [text, setText] = useState('');
+  const sizeLimit = 255;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleBeforeAddUndo = (evt: any, editor: any) => {
+    const length = editor.getContent({ format: 'text' }).length;
+    if (length > sizeLimit) {
+      evt.preventDefault();
+    }
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handelOnSubmit = (event: any) => {
+    event.preventDefault();
+    console.log('input text value :', value);
+  };
+
   return (
-    <>
+    <form onSubmit={handelOnSubmit}>
       <Editor
         apiKey={'a3y6602vgjj9ashpp5viyohj4sjbbdci6g3aqpaimp4ua8jc'}
         init={{
-          height: 500,
+          height: 250,
           menubar: true,
           plugins: [
             'advlist',
@@ -25,6 +45,8 @@ export const TextEditor = () => {
             'searchreplace',
             'table',
             'visualblocks',
+            'visualchars',
+            'wordcount',
           ],
           toolbar:
             'undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
@@ -36,7 +58,20 @@ export const TextEditor = () => {
           default_link_target: '_blank',
           link_default_protocol: 'https',
         }}
+        value={value}
+        onInit={(_evt, editor) => {
+          setText(editor.getContent({ format: 'text' }));
+          editor.getBody().style.backgroundColor = '#dddddd';
+        }}
+        onEditorChange={(newValue, editor) => {
+          setValue(newValue);
+          setText(editor.getContent({ format: 'text' }));
+        }}
+        onBeforeAddUndo={handleBeforeAddUndo}
+        disabled={isDisabled}
       />
-    </>
+      <p>Nombre de caratères restants: {sizeLimit - text.length}</p>
+      <SubmitButton isLoadingSubmit={false} buttonLabel={'Enregistrer'} />
+    </form>
   );
 };

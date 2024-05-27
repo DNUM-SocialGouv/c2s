8547c632907c5d7c6d3e7144912a4b3e24ca284e
Tlili <sami.tlili@sg.social.gouv.fr>
2024-05-27 20:22:56 +0200

    feat: adding control to form

--- src/page/infoTab/InfoTab.tsx
+++ src/page/infoTab/InfoTab.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
-import { fetchMembreInfo } from '@/page/infoTab/action.ts';
+import { fetchMembreInfo, updateMembreInfo } from '@/page/infoTab/action.ts';
 import { useDispatch, useSelector } from 'react-redux';
-//import { useDeleteAccount } from '@/hooks/useDeleteAccount.tsx';
 import { useForm, FormProvider } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { iMembreData } from '@/domain/OcInformationTab';
 import { Avatar } from '@/components/common/svg/Avatar';
 import { FormInputWithYup } from '@/components/common/input/FormInputWithYup';
+import { useDeleteAccount } from '@/hooks/useDeleteAccount';
 
 interface InfoTabProps {
   setActionAndOpenModal: () => void;
@@ -21,54 +21,73 @@ interface RootState {
 }
 
 const schema = yup.object().shape({
-  nom: yup.string().required('Le nom est requis'),
-  prenom: yup.string().required('Le prénom est requis'),
-  telephone: yup.string().required('Le numéro de telephone est requis'),
-  fonction: yup.string().required('La fonction est requise'),
+  nom: yup.string().required('*Le nom est requis').min(2, 'Doit contenir au moins 2 caractères'),
+  prenom: yup.string().required('*Le prénom est requis').min(2),
+  telephone: yup.string().required('*Le numéro de telephone et requis').min(10, '*Le champs doit contenir 10 caractères'),
+  fonction: yup.string().required('*La fonction est requise'),
+  nouveauMdp: yup
+    .string()
+    .required('*Le mot de passe est requis').min(12, 'Doit contenir 12 caractères'),
+  confirmMdp: yup
+    .string()
+    .required('*la confirmation est requise').oneOf([yup.ref('nouveauMdp')],'*Les mots de passes doivent être identiques'),
 });
 
 const InfoTab = ({ setActionAndOpenModal }: InfoTabProps) => {
   const dispatch = useDispatch();
-  // const membreDataRedux = useSelector(
-  //   (state: RootState) => state.membreInfo.membreData
-  // );
+  const membreDataRedux = useSelector(
+    (state: RootState) => state.membreInfo.membreData
+  );
 
   const { error } = useSelector((state: RootState) => state.membreInfo);
 
-  //const { setAccountToDelete } = useDeleteAccount();
+  const { setAccountToDelete } = useDeleteAccount();
 
-  const methods = useForm({
+  const formDefaultValues = { 
+    nom: membreDataRedux?.nom || '',
+    prenom: membreDataRedux?.prenom || '',
+    telephone: membreDataRedux?.telephone || '',
+    fonction: membreDataRedux?.fonction || '',
+    confirmMdp: '',
+    nouveauMdp: '',
+  }
+
+  const methods = useForm<{ 
+    nom: string;
+    prenom: string;
+    telephone: string;
+    fonction: string;
+    confirmMdp: string;
+    nouveauMdp: string;
+  }>({
+    defaultValues: formDefaultValues,
     resolver: yupResolver(schema),
   });
+
   const { handleSubmit } = methods;
 
-  const onSubmit = (data: unknown) => {
-    // dispatch(updateMembreInfo(data));
-    console.log(data);
+  const onSubmit = (data: {
+    nom: string;
+    prenom: string;
+    telephone: string;
+    fonction: string;
+    confirmMdp: string;
+    nouveauMdp: string;
+  }) => {
+    if (membreDataRedux && membreDataRedux.membreId) {
+      const membreToUpdate = {
+        membreId: membreDataRedux.membreId,
+        login: membreDataRedux.login,
+        nom: data.nom,
+        prenom: data.prenom,
+        fonction: data.fonction,
+        email: membreDataRedux.email,
+        telephone: data.telephone,
+        password: data.nouveauMdp,
+      };
+      dispatch(updateMembreInfo(membreToUpdate));
+    }
   };
-
-  // const [membreData, setMembreData] = useState<iMembreData>({
-  //   membreId: '',
-  //   login: '',
-  //   nom: '',
-  //   prenom: '',
-  //   fonction: '',
-  //   email: '',
-  //   telephone: '',
-  //   password: '',
-  // });
-
-  // useEffect(() => {
-  //   if (membreDataRedux) {
-  //     setMembreData(membreDataRedux);
-  //     const { membreId, login, email } = membreDataRedux;
-  //     setAccountToDelete({ membreId, login, email });
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
-  // const [isMatch, setIsMatch] = useState(true);
-  // const [confirmPassword, setConfirmPassword] = useState('');
 
   useEffect(() => {
     const login = localStorage.getItem('login');
@@ -106,76 +125,47 @@ const InfoTab = ({ setActionAndOpenModal }: InfoTabProps) => {
                 <FormInputWithYup label="Nom" name="nom" />
                 <FormInputWithYup label="Prénom" name="prenom" />
                 <FormInputWithYup label="Fonction" name="fonction" />
-
                 <div className="form-group">
                   <div className="mb-8 mt-8 h-px bg-gray-300 flex-none order-2 self-stretch flex-grow-0"></div>
                   <h5 className="fr-h5">Contacts</h5>
                 </div>
-                {/* <FormInput
-                label="E-mail"
-                name="email"
-                value={membreData.email || ''}
-                onChange={handleChange}
-                isDisabled={true}
-                /> */}
-                {/* <FormInput
-                label="Téléphone"
-                name="telephone"
-                value={membreData.telephone || ''}
-                onChange={handleChange}
-              /> */}
+                <FormInputWithYup
+                  label="E-mail"
+                  name="email"
+                  isDisabled={true}
+                />
+                <FormInputWithYup label="Téléphone" name="telephone" />
                 <div className="form-group">
                   <div className="mb-8 mt-8 h-px bg-gray-300 flex-none order-2 self-stretch flex-grow-0"></div>
                   <h5 className="fr-h5">Mot de passe</h5>
                 </div>
                 <div className="form-group mb-6">
-                  <label className="fr-label" htmlFor="password">
-                    Nouveau mot de passe
+                  <div
+                    className="fr-input-wrap"
+                    style={{ position: 'relative' }}
+                  >
+                    <FormInputWithYup
+                      label="Nouveau mot de passe"
+                      name="nouveauMdp"
+                      inputType='password'
+                    />
                     <span className="fr-hint-text">
                       12 caractères, composé de chiffres, lettres et caractères
                       spéciaux.
                     </span>
-                  </label>
-                  <div
-                    className="fr-input-wrap"
-                    style={{ position: 'relative' }}
-                  >
-                    {/* <input
-                    className="fr-input"
-                    aria-describedby="text-input-icon-messages"
-                    data-testid="password"
-                    id="password"
-                    name="password"
-                    type="password"
-                    onChange={handlePasswordChange}
-                    value={membreData.password || ''}
-                  /> */}
                   </div>
                 </div>
-
                 <div className="form-group mb-6">
-                  <label className="fr-label" htmlFor="confirmPassword">
-                    Confirmation du nouveau mot de passe
-                  </label>
                   <div
                     className="fr-input-wrap"
                     style={{ position: 'relative' }}
                   >
-                    {/* <input
-                    className="fr-input"
-                    aria-describedby="text-input-icon-messages"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    type="password"
-                    onChange={handleConfirmPasswordChange}
-                    value={confirmPassword}
-                  /> */}
+                    <FormInputWithYup
+                      label="Nouveau mot de passe"
+                      name="confirmMdp"
+                      inputType='password'
+                    />
                   </div>
-                  {/* {!isMatch && (
-                  <p className="fr-error-text">
-                    Les mots de passe ne sont pas identiques.
-                  </p>
-                )} */}
                 </div>
                 <div className="flex justify-between w-full md:flex-row">
                   <button
@@ -188,7 +178,16 @@ const InfoTab = ({ setActionAndOpenModal }: InfoTabProps) => {
                   <button
                     type="button"
                     className="fr-btn fr-btn--sm fr-btn--tertiary"
-                    onClick={() => setActionAndOpenModal()}
+                    onClick={() => {
+                      if (membreDataRedux && membreDataRedux.membreId) {
+                        setAccountToDelete({
+                          membreId: membreDataRedux?.membreId,
+                          login: membreDataRedux?.login,
+                          email: membreDataRedux?.email,
+                        });
+                        setActionAndOpenModal();
+                      }
+                    }}
                   >
                     Supprimer mon compte
                   </button>

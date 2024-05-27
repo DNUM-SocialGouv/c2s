0f6670d584ce9a6fafb520253ac868fb69a7a8cd
Tlili <sami.tlili@sg.social.gouv.fr>
2024-05-27 11:43:00 +0200

    chore: adding and config ay11 plugin for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,20 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jsx-a11y'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'jsx-a11y/no-static-element-interactions': 'warn',
+    'jsx-a11y/no-noninteractive-element-interactions': 'warn',
   },
 };

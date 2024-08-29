9cdb24391d2081c14ded00dca721321fe8bc57ad
Tlili <sami.tlili@sg.social.gouv.fr>
2024-08-29 11:33:22 +0200

    fix: fixing tests and disable next page btn when last page

--- src/page/etablishmentTab/pagination/Pagination.test.tsx
+++ src/page/etablishmentTab/pagination/Pagination.test.tsx
@@ -1,9 +1,8 @@
-// Pagination.test.tsx
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { axe, toHaveNoViolations } from 'jest-axe';
-import { Pagination } from './Pagination';
 import { COMMON } from '@/wording';
+import Pagination from './Pagination';
 
 expect.extend(toHaveNoViolations);
 
@@ -28,16 +27,12 @@ describe('Pagination', () => {
     currentPage: number,
     totalPages: number,
     onPageChange: jest.Mock,
-    onClickPrev?: jest.Mock,
-    onClickNext?: jest.Mock
   ) => {
     render(
       <Pagination
         currentPage={currentPage}
         totalPages={totalPages}
         onPageChange={onPageChange}
-        onClickPrev={onClickPrev}
-        onClickNext={onClickNext}
       />
     );
   };
@@ -53,26 +48,11 @@ describe('Pagination', () => {
     setup(1, 10, jest.fn());
 
     expect(screen.getByText(COMMON.prevPage)).toBeInTheDocument();
-    expect(screen.getByText(COMMON.nextPage)).toBeInTheDocument();
-  });
-
-  it('should call onClickNext when the next button is clicked', () => {
-    const onClickNext = jest.fn();
-    setup(1, 10, jest.fn(), undefined, onClickNext);
-
-    fireEvent.click(screen.getByText(COMMON.nextPage));
-    expect(onClickNext).toHaveBeenCalled();
-  });
-
-  it('should call onClickPrev when the previous button is clicked', () => {
-    const onClickPrev = jest.fn();
-    setup(2, 10, jest.fn(), onClickPrev, undefined);
-
-    fireEvent.click(screen.getByText(COMMON.prevPage));
-    expect(onClickPrev).toHaveBeenCalled();
+    expect(screen.getByText('Page suivante')).toBeInTheDocument();
   });
 
   it('should call onPageChange when a page number is clicked', () => {
+    
     const onPageChange = jest.fn();
     setup(1, 10, onPageChange);
 
@@ -80,20 +60,22 @@ describe('Pagination', () => {
     expect(onPageChange).toHaveBeenCalledWith(2);
   });
 
-  it('should disable the next button when on the last page', () => {
+  it('should hide the next button when on the last page', () => {
+    // WHEN
     setup(10, 10, jest.fn());
+    // THEN
+    expect(screen.queryByText('Page suivante')).not.toBeInTheDocument();
 
-    const nextButton = screen.getByText(COMMON.nextPage);
-    expect(nextButton).toBeDisabled();
-    expect(nextButton).toHaveAttribute('aria-disabled', 'true');
   });
 
   it('should disable the previous button when on the first page', () => {
     setup(1, 10, jest.fn());
 
     const prevButton = screen.getByText(COMMON.prevPage);
-    expect(prevButton).toBeDisabled();
-    expect(prevButton).toHaveAttribute('aria-disabled', 'true');
+    waitFor(() => {
+      expect(prevButton).toBeDisabled();
+      expect(prevButton).toHaveAttribute('aria-disabled', 'true');
+    })
   });
 
   it('should show ellipsis when there are more pages than maxPagesToShow', () => {
@@ -102,3 +84,4 @@ describe('Pagination', () => {
     expect(screen.getAllByText('...')).toHaveLength(2);
   });
 });
+

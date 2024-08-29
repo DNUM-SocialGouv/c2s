804b1e54db8fbbfe4ead93f34c0834b91c400847
Tlili <sami.tlili@sg.social.gouv.fr>
2024-08-29 12:58:48 +0200

    test: adding tests to editor component

--- src/components/common/textEditor/textEditor.test.tsx
+++ src/components/common/textEditor/textEditor.test.tsx
@@ -1,81 +1,122 @@
+import '@testing-library/jest-dom';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { TextEditor } from './TextEditor';
 import { axiosInstance } from '@/RequestInterceptor';
+import MockAdapter from 'axios-mock-adapter';
+
+const moderatorMessageFixture = {
+  id: 0,
+  contenu: 'string',
+  groupe: 'CAISSE',
+  dateCrea: '2024-08-29T10:35:53.810Z',
+  dateMaj: '2024-08-29T10:35:53.810Z',
+};
+
+beforeAll(async () => {
+  const mock = new MockAdapter(axiosInstance, { delayResponse: 200 });
+  mock.onGet('/moderateur/messages/OC').reply(200, {
+    data: moderatorMessageFixture,
+  });
+});
 
 describe('TextEditor', () => {
   it('should render the editor', () => {
     // GIVEN
-    const groupe = 'sample-group';
+    const groupe = 'OC';
 
     // WHEN
     render(<TextEditor groupe={groupe} />);
 
     // THEN
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    waitFor(() => {
+      expect(screen.getByRole('textbox')).toBeInTheDocument();
+    });
   });
 
   it('should update the value when typing in the editor', () => {
     // GIVEN
-    const groupe = 'sample-group';
+    const groupe = 'OC';
     render(<TextEditor groupe={groupe} />);
-    const editor = screen.getByRole('textbox');
+    let editor: Node | Window;
+
+    waitFor(() => {
+      editor = screen.getByRole('textbox');
+    });
 
     // WHEN
-    fireEvent.change(editor, { target: { value: 'Sample text' } });
+    waitFor(() => {
+      fireEvent.change(editor, { target: { value: 'mon message' } });
+    });
 
     // THEN
-    expect(editor).toHaveValue('Sample text');
+    waitFor(() => {
+      expect(editor).toHaveValue('mon message');
+    });
   });
 
   it('should call the API when submitting the form', async () => {
     // GIVEN
-    const groupe = 'sample-group';
+    const groupe = 'OC';
     render(<TextEditor groupe={groupe} />);
-    const editor = screen.getByRole('textbox');
+    let editor: Node | Window;
+
+    waitFor(() => {
+      editor = screen.getByRole('textbox');
+    });
+
     const submitButton = screen.getByText('Enregistrer');
 
-    // Mock the axiosInstance.post method
-    jest.spyOn(window.axiosInstance, 'post').mockResolvedValueOnce({
-      data: { contenu: 'Sample response' },
+    jest.spyOn(axiosInstance, 'post').mockResolvedValueOnce({
+      data: { contenu: 'mon message' },
     });
 
     // WHEN
-    fireEvent.change(editor, { target: { value: 'Sample text' } });
+    waitFor(() => {
+      fireEvent.change(editor, { target: { value: 'mon message' } });
+    });
+
     fireEvent.click(submitButton);
 
     // THEN
-    await waitFor(() => {
-      expect(window.axiosInstance.post).toHaveBeenCalledWith(
+    waitFor(() => {
+      expect(axiosInstance.post).toHaveBeenCalledWith(
         '/moderateur/messages',
         expect.any(String),
         { withCredentials: true }
       );
-      expect(editor).toHaveValue('Sample response');
+      expect(editor).toHaveValue('mon message');
     });
   });
 
   it('should display an error message when the API call fails', async () => {
     // GIVEN
-    const groupe = 'sample-group';
+    const groupe = 'OC';
     render(<TextEditor groupe={groupe} />);
-    const editor = screen.getByRole('textbox');
+    let editor: Node | Window;
+
+    waitFor(() => {
+      editor = screen.getByRole('textbox');
+    });
+
     const submitButton = screen.getByText('Enregistrer');
 
-    // Mock the axiosInstance.post method to simulate an error
-    jest.spyOn(window.axiosInstance, 'post').mockRejectedValueOnce({
+    jest.spyOn(axiosInstance, 'post').mockRejectedValueOnce({
       response: { status: 500 },
     });
 
     // WHEN
-    fireEvent.change(editor, { target: { value: 'Sample text' } });
+    waitFor(() => {
+      fireEvent.change(editor, { target: { value: 'mon message' } });
+    });
+
     fireEvent.click(submitButton);
 
     // THEN
-    await waitFor(() => {
+    waitFor(() => {
       expect(
         screen.getByText('Vous avez depassé le nombre de caractères authorisés')
       ).toBeInTheDocument();
-      expect(editor).toHaveValue('Sample text');
+      expect(editor).toHaveValue('mon message');
     });
   });
 });

62725eeea94e08de7bec6e107a561b7cbbfde89a
Sami Tlili <user@Ordinateur-portable-de-dnum-admin.local>
2024-10-03 15:08:15 +0200

    feat: adding caisse home page

---
+++ src/components/caisseAccueil/CaisseAccueil.tsx
@@ -0,0 +1,50 @@
+import { AccueilCitation } from '../common/accueilCitation/AccueilCitation';
+import './OcAccueil.css';
+import { Separator } from '../common/svg/Seperator';
+import { AccueilLinks } from '../common/accueilLinks/AccueilLinks';
+import { useContext, useEffect } from 'react';
+import { axiosInstance } from '@/RequestInterceptor';
+import { OcWelcomePageContext } from '@/contexts/OcWelcomeContext';
+import { ocWelcomeMessageMapper } from '@/utils/ocWelcomeMessage.mapper';
+import { WelcomeAPIResponse } from '@/domain/OcAccueil';
+import { LoginContext } from '@/contexts/LoginContext';
+import { Loader } from '../common/loader/Loader';
+import { AccueilHeader } from '../common/accueilHeader/AccueilHeader';
+
+export const CaisseAccueil = () => {
+  const context = useContext(OcWelcomePageContext);
+  const { isLogged } = useContext(LoginContext);
+
+  useEffect(() => {
+    if (isLogged) {
+      axiosInstance
+        .get<WelcomeAPIResponse>('/partenaire/welcome')
+        .then((response) => {
+          const message = ocWelcomeMessageMapper(response.data.messageAccueil);
+          context.setMessage(message);
+          context.setLinks(response.data.ressourceFiles);
+        });
+    }
+    // Suite au renommage de OcLoginContext en LoginContext,
+    // inclure le context dans le tableau de dépendances du useEffect,
+    // Entraine une boucle infinie/erreur.
+    // Afin de corriger cela, il est nécessaire de supprimer le context du tableau de dépendances.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLogged]);
+
+  return (
+    <>
+      {!isLogged ? (
+        <Loader />
+      ) : (
+        <div className="fr-container--fluid" data-testid="CaisseAccueil">
+          <AccueilHeader />
+          <Separator />
+          <AccueilCitation />
+          <Separator />
+          <AccueilLinks />
+        </div>
+      )}
+    </>
+  );
+};

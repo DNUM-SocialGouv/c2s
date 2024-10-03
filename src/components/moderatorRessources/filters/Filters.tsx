6a761eae9bded74a51561272dc1e03465f825fd1
Sami Tlili <user@Ordinateur-portable-de-dnum-admin.local>
2024-10-03 05:19:44 +0200

    feat: adding filters component(disabled)

---
+++ src/components/moderatorRessources/filters/Filters.tsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import { Thematique } from '../thematiquesForm/ThematiquesForm';
+import '@/components/common/filters/Filters.css';
+import { Search } from '@/components/common/svg/Search';
+
+interface FiltersProps {
+  thematiquesList: Thematique[];
+}
+
+export const Filters: React.FC<FiltersProps> = ({ thematiquesList }) => {
+  const [thematique, setThematique] = useState('');
+
+  const cible = ['Tout afficher', 'Organisme complémentaire', 'Caisse'];
+
+  const handleThematiqueChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setThematique(event.target.value);
+  };
+
+  return (
+    <div
+      className="fr-grid-row filters flex items-center justify-between"
+      style={{ width: '100%' }}
+    >
+      <div className="filters__filter">
+        <div className="fr-input-group">
+          <label className="fr-label" htmlFor="text-input-icon">
+            Rechercher
+          </label>
+          <div className="fr-input-wrap fr-icon-alert-line search">
+            <input
+              className="fr-input"
+              aria-describedby="text-input-icon-messages"
+              id="text-input-icon"
+              type="text"
+              placeholder="Mots clés"
+              aria-label="Search input"
+              disabled
+            />
+            <button
+              className="fr-btn search__button"
+              title="Label bouton"
+              onClick={() => console.log('search')}
+              disabled
+            >
+              <Search />
+            </button>
+          </div>
+          <div
+            className="fr-messages-group"
+            id="text-input-icon-messages"
+            aria-live="assertive"
+          ></div>
+        </div>
+      </div>
+      <div className="filters__filter">
+        <div className="fr-select-group">
+          <label className="fr-label" htmlFor="thematique">
+            Thématique
+          </label>
+          <select
+            className="fr-select"
+            id="thematique"
+            name="thematique"
+            onChange={(event) => handleThematiqueChange(event)}
+            value={thematique}
+            disabled
+          >
+            <option value="Tout afficher">Tout afficher</option>
+            {thematiquesList.length > 0 &&
+              thematiquesList.map((item, index) => (
+                <option key={index} value={item.titre}>
+                  {item.titre}
+                </option>
+              ))}
+          </select>
+        </div>
+      </div>
+      <div className="filters__filter">
+        <div className="fr-select-group">
+          <label className="fr-label" htmlFor="public">
+            Public
+          </label>
+          <select
+            className="fr-select"
+            id="public"
+            name="public"
+            value=""
+            disabled
+          >
+            {cible.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/moderatorRessources/ressourcesForm/RessourcesForm.tsx
+++ src/components/moderatorRessources/ressourcesForm/RessourcesForm.tsx
@@ -1,9 +1,11 @@
 import './RessourcesForm.css';
 import { ThematiquesForm } from '../thematiquesForm/ThematiquesForm';
+import { Filters } from '../filters/Filters';
 
 export const RessourceForm = () => {
   return (
     <>
+      <Filters thematiquesList={[]} />
       <ThematiquesForm />
     </>
   );

31158d1922101b0b3a2305205dad542cbf6c96b2
Tlili <sami.tlili@sg.social.gouv.fr>
2024-10-11 16:18:03 +0200

    test: updating tests

---
+++ src/components/common/input/CheckboxGroup.test.tsx
@@ -0,0 +1,52 @@
+import React, { ReactNode } from 'react';
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useForm, FormProvider } from 'react-hook-form';
+import { CheckboxGroup } from './CheckboxGroup';
+
+const options = [
+  { id: '1', label: 'Thématique 1' },
+  { id: '2', label: 'Thématique 2' },
+  { id: '3', label: 'Thématique 3' },
+];
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+  const methods = useForm();
+  return <FormProvider {...methods}>{children}</FormProvider>;
+};
+
+describe('CheckboxGroup', () => {
+  it('should render the checkbox group with options', () => {
+    render(
+      <Wrapper>
+        <CheckboxGroup name="test" options={options} />
+      </Wrapper>
+    );
+    // THEN
+    options.forEach((option) => {
+      const checkbox = screen.getByLabelText(option.label) as HTMLInputElement;
+      expect(checkbox).toBeInTheDocument();
+      expect(checkbox.checked).toBe(false);
+    });
+  });
+
+  it('should update the checked state when checkbox is clicked', () => {
+    render(
+      <Wrapper>
+        <CheckboxGroup name="test" options={options} />
+      </Wrapper>
+    );
+
+    const checkbox = screen.getByLabelText('Thématique 1') as HTMLInputElement;
+    // THEN
+    expect(checkbox.checked).toBe(false);
+
+    userEvent.click(checkbox);
+    waitFor(() => expect(checkbox.checked).toBe(true));
+  });
+});

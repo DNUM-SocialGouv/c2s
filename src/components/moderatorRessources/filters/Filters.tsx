5cb782c2d8e1aeb4bac6b8cad7c4dd62e0ea7fff
Tlili <sami.tlili@sg.social.gouv.fr>
2024-10-21 14:42:12 +0200

    refacto: retour revue de code. Afficher un message d'erreur

--- src/components/moderatorRessources/filters/Filters.tsx
+++ src/components/moderatorRessources/filters/Filters.tsx
@@ -6,10 +6,13 @@ import { COMMON, MODERATOR_RESOURCES_FILTERS } from '@/wording';
 import { ModeratorRessourcesContext } from '@/contexts/ModeratorRessourceContext';
 import { ModeratorThematiqueFromAPI } from '@/domain/ModeratorRessources';
 import { axiosInstance } from '@/RequestInterceptor';
+import { AxiosError } from 'axios';
+import { Alert } from '@/components/common/alert/Alert';
 
 export const Filters: React.FC = () => {
   const [selectedThematiqueTitle, setSelectedThematiqueTitle] = useState('');
   const cible = ['Tout afficher', 'Organisme complémentaire', 'Caisse'];
+  const [error, setError] = useState<boolean>(false);
 
   const { thematiques, setThematiques } = useContext(
     ModeratorRessourcesContext
@@ -23,6 +26,10 @@ export const Filters: React.FC = () => {
       .then((response) => {
         const thematiquesFromAPI = response.data;
         setThematiques(thematiquesFromAPI);
+      })
+      .catch((error: AxiosError) => {
+        console.error(error);
+        setError(true);
       });
   };
 
@@ -42,86 +49,95 @@ export const Filters: React.FC = () => {
   };
 
   return (
-    <div className="fr-grid-row filters flex items-center justify-between w-full">
-      <div className="filters__filter">
-        {/* Recherche */}
-        <div className="fr-input-group">
-          <label className="fr-label" htmlFor="text-input-icon">
-            {COMMON.rechercher}
-          </label>
-          <div className="fr-input-wrap fr-icon-alert-line search">
-            <input
-              className="fr-input"
-              aria-describedby="text-input-icon-messages"
-              id="text-input-icon"
-              type="text"
-              placeholder="Mots clés"
-              aria-label="Search input"
-              disabled
-            />
-            <button
-              className="fr-btn search__button"
-              title="Label bouton"
-              onClick={() => console.log('search')}
+    <>
+      <div className="fr-grid-row filters flex items-center justify-between w-full">
+        <div className="filters__filter">
+          {/* Recherche */}
+          <div className="fr-input-group">
+            <label className="fr-label" htmlFor="text-input-icon">
+              {COMMON.rechercher}
+            </label>
+            <div className="fr-input-wrap fr-icon-alert-line search">
+              <input
+                className="fr-input"
+                aria-describedby="text-input-icon-messages"
+                id="text-input-icon"
+                type="text"
+                placeholder="Mots clés"
+                aria-label="Search input"
+                disabled
+              />
+              <button
+                className="fr-btn search__button"
+                title="Label bouton"
+                onClick={() => console.log('search')}
+                disabled
+              >
+                <Search />
+              </button>
+            </div>
+            <div
+              className="fr-messages-group"
+              id="text-input-icon-messages"
+              aria-live="assertive"
+            ></div>
+          </div>
+        </div>
+        <div className="filters__filter">
+          {/* Thématique */}
+          <div className="fr-select-group">
+            <label className="fr-label" htmlFor="thematique">
+              {MODERATOR_RESOURCES_FILTERS.thematique}
+            </label>
+            <select
+              className="fr-select"
+              id="thematique"
+              name="thematique"
+              onChange={(event) => handleThematiqueChange(event)}
+              value={selectedThematiqueTitle}
+            >
+              <option value="Tout afficher">
+                {' '}
+                {MODERATOR_RESOURCES_FILTERS.displayAll}
+              </option>
+              {thematiques.length > 0 &&
+                thematiques.map((item, index) => (
+                  <option key={index} value={item.titre}>
+                    {item.titre}
+                  </option>
+                ))}
+            </select>
+          </div>
+        </div>
+        {/* Cible */}
+        <div className="filters__filter">
+          <div className="fr-select-group">
+            <label className="fr-label" htmlFor="public">
+              {MODERATOR_RESOURCES_FILTERS.public}
+            </label>
+            <select
+              className="fr-select"
+              id="public"
+              name="public"
+              value=""
               disabled
             >
-              <Search />
-            </button>
-          </div>
-          <div
-            className="fr-messages-group"
-            id="text-input-icon-messages"
-            aria-live="assertive"
-          ></div>
-        </div>
-      </div>
-      <div className="filters__filter">
-        {/* Thématique */}
-        <div className="fr-select-group">
-          <label className="fr-label" htmlFor="thematique">
-            {MODERATOR_RESOURCES_FILTERS.thematique}
-          </label>
-          <select
-            className="fr-select"
-            id="thematique"
-            name="thematique"
-            onChange={(event) => handleThematiqueChange(event)}
-            value={selectedThematiqueTitle}
-          >
-            <option value="Tout afficher">
-              {' '}
-              {MODERATOR_RESOURCES_FILTERS.displayAll}
-            </option>
-            {thematiques.length > 0 &&
-              thematiques.map((item, index) => (
-                <option key={index} value={item.titre}>
-                  {item.titre}
+              {cible.map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
                 </option>
               ))}
-          </select>
+            </select>
+          </div>
         </div>
       </div>
-      {/* Cible */}
-      <div className="filters__filter">
-        <div className="fr-select-group">
-          <label className="fr-label" htmlFor="public">
-            {MODERATOR_RESOURCES_FILTERS.public}
-          </label>
-          <select
-            className="fr-select"
-            id="public"
-            name="public"
-            value=""
-            disabled
-          >
-            {cible.map((item, index) => (
-              <option key={index} value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
-    </div>
+      {error && (
+        <Alert
+          label="Erreur"
+          description="Une erreur est survenue lors de la récupération des données."
+          type="error"
+        />
+      )}
+    </>
   );
 };

28cdb60d29a2d8cbca96c5804a10ff2ec87e7566
Tlili <sami.tlili@sg.social.gouv.fr>
2024-10-29 14:44:17 +0100

    tests: refactoring moderator ressources tests

--- front/src/components/moderatorRessources/thematiquesForm/ThematiquesForm.test.tsx
+++ front/src/components/moderatorRessources/thematiquesForm/ThematiquesForm.test.tsx
@@ -10,26 +10,30 @@ import {
 } from '../../../utils/tests/moderatorRessources.fixtures.ts';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import { ocWelcomeAPIResponse } from '../../../utils/tests/ocWelcome.fixtures.ts';
+import { ModeratorRessourcesContext } from '@/contexts/ModeratorRessourceContext.tsx';
 
 expect.extend(toHaveNoViolations);
 
 describe('ThematiquesForm', () => {
   beforeAll(async () => {
     const mock = new MockAdapter(axiosInstance, { delayResponse: 2000 });
-    mock.onGet('/moderateur/message/oc').reply(200, {
+
+    mock.onGet('moderateur/fichiers/').reply(200, {
       ressources: moderatorRessources,
     });
 
     const mockRessourcesFiles = new MockAdapter(axiosInstance, {
       delayResponse: 200,
     });
+
     mockRessourcesFiles.onGet('/partenaire/welcome').reply(200, {
       users: ocWelcomeAPIResponse,
     });
 
     const mockRessourcesThematiques = new MockAdapter(axiosInstance, {
-      delayResponse: 2000,
+      delayResponse: 200,
     });
+
     mockRessourcesThematiques
       .onGet('/moderateur/fichiers/search?thematiqueId=1')
       .reply(200, {
@@ -38,37 +42,44 @@ describe('ThematiquesForm', () => {
   });
 
   it('should pass accessibility tests', async () => {
-    // WHEN
-    const { container } = render(<ThematiquesForm />);
+    // GIVEN
+    const { container } = render(
+      <ModeratorRessourcesContext.Provider
+        value={{
+          thematiques: moderatorThematiques,
+          setThematiques: () => undefined,
+        }}
+      >
+        <ThematiquesForm />
+      </ModeratorRessourcesContext.Provider>
+    );
     const results = await axe(container);
     // THEN
-    waitFor(() => {
-      expect(results).toHaveNoViolations();
-    });
+    expect(results).toHaveNoViolations();
   });
 
-  it('should render the form with the correct number of thematiques', () => {
+  it('should render the form with the correct number of thematiques', async () => {
     // WHEN
     const { container } = render(<ThematiquesForm />);
     const formElement = container.querySelector('form');
     expect(formElement).toBeInTheDocument();
     // THEN
-    waitFor(() => {
+    await waitFor(() => {
       const thematiqueElements = container.querySelectorAll('h3');
       expect(thematiqueElements.length).toBe(4);
     });
   });
 
-  it('should update the thematique title when input value changes', () => {
+  it('should update the thematique title when input value changes', async () => {
     // GIVEN
     render(<ThematiquesForm />);
-    waitFor(() => {
-      const inputElement = screen.getByLabelText(
-        'Thématique de la sélection'
-      ) as HTMLInputElement;
-      // WHEN
-      userEvent.type(inputElement, 'Ma nouvelle thématique');
-      // THEN
+    const inputElement = screen.getByLabelText(
+      'Nom de la thématique'
+    ) as HTMLInputElement;
+    // WHEN
+    userEvent.type(inputElement, 'Ma nouvelle thématique');
+    // THEN
+    await waitFor(() => {
       expect(inputElement.value).toBe('Ma nouvelle thématique');
     });
   });

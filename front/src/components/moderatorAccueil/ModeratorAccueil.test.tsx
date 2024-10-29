36c7a1789ec51475ea8dcd9f68310ee633fe8b0d
olivier-rabot <olivier.rabot@sg.social.gouv.fr>
2024-10-29 14:58:25 +0100

    feat: moderateur hp

---
+++ front/src/components/moderatorAccueil/ModeratorAccueil.test.tsx
@@ -0,0 +1,61 @@
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import { ModeratorAccueil } from '../../components/moderatorAccueil/ModeratorAccueil';
+import { LoginContext } from '../../contexts/LoginContext';
+import { axiosInstance } from '../../RequestInterceptor';
+import { MODERATEUR_ACCUEIL } from '../../wording';
+import type { ModeratorAccueilMetricsTypes } from '../../domain/ModerateurAccueil';
+
+jest.mock('@/RequestInterceptor', () => ({
+  axiosInstance: {
+    get: jest.fn(),
+  },
+}));
+
+describe('ModeratorAccueil', () => {
+  const mockMetrics: ModeratorAccueilMetricsTypes = {
+    membresAModerer: ['Laurent Duc', 'Marie Dupont'],
+    membresActifCount: 5,
+    organisationsActifCount: 3,
+    pointAccueilActifCount: 2,
+  };
+
+  const renderComponent = (isLogged = false) =>
+    render(
+      <LoginContext.Provider
+        value={{
+          isLogged,
+          setIsLogged: () => undefined,
+        }}
+      >
+        <ModeratorAccueil />
+      </LoginContext.Provider>
+    );
+
+  beforeEach(() => {
+    (axiosInstance.get as jest.Mock).mockResolvedValue({
+      data: mockMetrics,
+    });
+  });
+
+  it('should render the Loader while data is loading', () => {
+    renderComponent();
+
+    expect(screen.getByTestId('loader')).toBeInTheDocument();
+  });
+
+  it('should display the correct title and badge in the Tuile component', async () => {
+    renderComponent(true);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          MODERATEUR_ACCUEIL.newMembersNumber(
+            mockMetrics.membresAModerer.length
+          )
+        )
+      ).toBeInTheDocument();
+      expect(screen.getByText(MODERATEUR_ACCUEIL.badge)).toBeInTheDocument();
+    });
+  });
+});

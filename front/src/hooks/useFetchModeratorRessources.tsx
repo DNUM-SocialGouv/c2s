2be327c44a1a5d34c32816c50c109cdf16bc8c58
Tlili <sami.tlili@sg.social.gouv.fr>
2024-10-30 12:19:12 +0100

    refacto: using custom hook to fetch moderator ressources

---
+++ front/src/hooks/useFetchModeratorRessources.tsx
@@ -0,0 +1,27 @@
+import { ModeratorRessourcesContext } from '@/contexts/ModeratorRessourceContext';
+import { ModeratorThematiqueFromAPI } from '@/domain/ModeratorRessources';
+import { axiosInstance } from '@/RequestInterceptor';
+import { useEffect, useContext } from 'react';
+
+export const useFetchModeratorRessources = () => {
+  const { setThematiques } = useContext(ModeratorRessourcesContext);
+
+  useEffect(() => {
+    const fetchRessources = async () => {
+      try {
+        const response = await axiosInstance.get<ModeratorThematiqueFromAPI[]>(
+          '/moderateur/thematiques',
+          {
+            withCredentials: true,
+          }
+        );
+        const data: ModeratorThematiqueFromAPI[] = response.data;
+        setThematiques(data);
+      } catch (error) {
+        console.error('Error fetching ressources:', error);
+      }
+    };
+
+    fetchRessources();
+  }, [setThematiques]);
+};

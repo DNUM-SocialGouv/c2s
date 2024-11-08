78be4ed2f72036607ecbdab23c504442501cc1f2
Tlili <sami.tlili@sg.social.gouv.fr>
2024-11-08 10:37:17 +0100

    tests: adding missing testss to pages

--- front/src/page/partnerHomePage/ParthnerHomePage.test.tsx
+++ front/src/page/partnerHomePage/ParthnerHomePage.test.tsx
@@ -1,6 +1,5 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-//import { getByText, waitFor } from '@testing-library/dom';
 import PartnerHomePage from './PartnerHomePage.tsx';
 import { AccountContext } from '../../contexts/AccountContext.tsx';
 import { axiosInstance } from '../../RequestInterceptor.tsx';
@@ -8,6 +7,7 @@ import { ocWelcomeAPIResponse } from '../../utils/tests/ocWelcome.fixtures.ts';
 import fetchMock from 'jest-fetch-mock';
 import MockAdapter from 'axios-mock-adapter';
 import { LoginContext } from '../../contexts/LoginContext.tsx';
+import { ActiveTabProvider } from '@/contexts/ActiveTabContext.tsx';
 
 fetchMock.dontMock();
 const mock = new MockAdapter(axiosInstance, { delayResponse: 2000 });
@@ -37,141 +37,203 @@ jest.mock('@react-keycloak/web', () => ({
   }),
 }));
 
+afterEach(() => {
+  cleanup();
+});
+
 describe('PartnerHomePage', () => {
   const deleteAction = jest.fn();
   const setAccountToDelete = jest.fn();
   const accountToDelete = { membreId: 0, email: '' };
 
-  beforeEach(async () => {
-    jest.mock('../../hooks/useDeleteAccount.tsx', () => ({
-      useDeleteAccount: jest.fn().mockReturnValue({ deleteAction }),
+  describe('should render the partner home page', () => {
+    beforeEach(async () => {
+      jest.mock('../../hooks/useDeleteAccount.tsx', () => ({
+        useDeleteAccount: jest.fn().mockReturnValue({ deleteAction }),
+      }));
+    });
+  
+    it('should render the loader if user is not logged', () => {
+      render(
+        <LoginContext.Provider
+          value={{
+            isLogged: false,
+            setIsLogged: () => undefined,
+          }}
+        >
+          <AccountContext.Provider
+            value={{ setAccountToDelete, accountToDelete, deleteAction }}
+          >
+            <PartnerHomePage />
+          </AccountContext.Provider>
+        </LoginContext.Provider>
+      );
+      expect(screen.getByRole('alert')).toBeInTheDocument();
+    }); 
+    
+    it('should render the partner information', () => {
+      const { getAllByText } = render(
+        <LoginContext.Provider
+          value={{
+            isLogged: true,
+            setIsLogged: () => undefined,
+          }}
+        >
+          <AccountContext.Provider
+            value={{ setAccountToDelete, accountToDelete, deleteAction }}
+          >
+            <PartnerHomePage />
+          </AccountContext.Provider>
+        </LoginContext.Provider>
+      );
+      const partnerInfo = getAllByText('Mes informations');
+      expect(partnerInfo[0]).toBeInTheDocument();
+    });
+  
+    it('should render the partner historique', () => {
+      const { getAllByText } = render(
+        <LoginContext.Provider
+          value={{
+            isLogged: true,
+            setIsLogged: () => undefined,
+          }}
+        >
+          <AccountContext.Provider
+            value={{ setAccountToDelete, accountToDelete, deleteAction }}
+          >
+            <PartnerHomePage />
+          </AccountContext.Provider>
+        </LoginContext.Provider>
+      );
+      const partnerStats = getAllByText('Historique');
+      expect(partnerStats[0]).toBeInTheDocument();
+    });
+  
+    it('should render the partner Mon équipe', () => {
+      const { getAllByText } = render(
+        <LoginContext.Provider
+          value={{
+            isLogged: true,
+            setIsLogged: () => undefined,
+          }}
+        >
+          <AccountContext.Provider
+            value={{ setAccountToDelete, accountToDelete, deleteAction }}
+          >
+            <PartnerHomePage />
+          </AccountContext.Provider>
+        </LoginContext.Provider>
+      );
+      const partnerStats = getAllByText('Mon équipe');
+      expect(partnerStats[0]).toBeInTheDocument();
+    });
+  
+    it('should render the partner Mes établissements', () => {
+      const { getAllByText } = render(
+        <LoginContext.Provider
+          value={{
+            isLogged: true,
+            setIsLogged: () => undefined,
+          }}
+        >
+          <AccountContext.Provider
+            value={{ setAccountToDelete, accountToDelete, deleteAction }}
+          >
+            <PartnerHomePage />
+          </AccountContext.Provider>
+        </LoginContext.Provider>
+      );
+      const partnerStats = getAllByText('Mes établissements');
+      expect(partnerStats[0]).toBeInTheDocument();
+    });
+  
+    it('should render Mon équipe bloc', () => {
+      // GIVEN
+      render(
+        <LoginContext.Provider
+          value={{
+            isLogged: true,
+            setIsLogged: () => undefined,
+          }}
+        >
+          <AccountContext.Provider
+            value={{ setAccountToDelete, accountToDelete, deleteAction }}
+          >
+            <PartnerHomePage />
+          </AccountContext.Provider>
+        </LoginContext.Provider>
+      );
+  
+      // THEN
+      expect(screen.getAllByText('Mon équipe').length).toEqual(2);
+    });
+  });
+
+  describe('should navigate on click', () => {
+    jest.mock('../../hooks/useFetchPartenairesRessources.tsx', () => ({
+      useFetchPartenairesRessources: () => ({
+        loading: false,
+        error: null,
+      }),
     }));
-  });
 
-  it('should render the partner information', () => {
-    const { getAllByText } = render(
-      <LoginContext.Provider
-        value={{
-          isLogged: true,
-          setIsLogged: () => undefined,
-        }}
-      >
-        <AccountContext.Provider
-          value={{ setAccountToDelete, accountToDelete, deleteAction }}
+    it('should navigate to Ressources', async() => {
+      // GIVEN
+      const { getAllByText } = render(
+        <LoginContext.Provider
+          value={{
+            isLogged: true,
+            setIsLogged: () => undefined,
+          }}
         >
-          <PartnerHomePage />
-        </AccountContext.Provider>
-      </LoginContext.Provider>
-    );
-    const partnerInfo = getAllByText('Mes informations');
-    expect(partnerInfo[0]).toBeInTheDocument();
-  });
+          <AccountContext.Provider
+            value={{ setAccountToDelete, accountToDelete, deleteAction }}
+          >
+            <ActiveTabProvider>
+              <PartnerHomePage />
+            </ActiveTabProvider>
+          </AccountContext.Provider>
+        </LoginContext.Provider>
+      );
+  
+      expect(screen.getByText(`Le petit mot de l'équipe C2S`)).toBeInTheDocument();
+      const partnerRessources = getAllByText('Ressources');
+      // WHEN
+      fireEvent.click(partnerRessources[0]);
 
-  it('should render the partner historique', () => {
-    const { getAllByText } = render(
-      <LoginContext.Provider
-        value={{
-          isLogged: true,
-          setIsLogged: () => undefined,
-        }}
-      >
-        <AccountContext.Provider
-          value={{ setAccountToDelete, accountToDelete, deleteAction }}
+      // THEN
+      await waitFor(() => {
+        expect(screen.getByText('Référents Gestion C2S')).toBeInTheDocument();
+      });
+    });
+
+    it('should navigate to Mon équipe', async() => {
+      // GIVEN
+      const { getAllByText } = render(
+        <LoginContext.Provider
+          value={{
+            isLogged: true,
+            setIsLogged: () => undefined,
+          }}
         >
-          <PartnerHomePage />
-        </AccountContext.Provider>
-      </LoginContext.Provider>
-    );
-    const partnerStats = getAllByText('Historique');
-    expect(partnerStats[0]).toBeInTheDocument();
-  });
+          <AccountContext.Provider
+            value={{ setAccountToDelete, accountToDelete, deleteAction }}
+          >
+            <ActiveTabProvider>
+              <PartnerHomePage />
+            </ActiveTabProvider>
+          </AccountContext.Provider>
+        </LoginContext.Provider>
+      );
+  
+      const partnerTeam = getAllByText('Mon équipe');
+      // WHEN
+      fireEvent.click(partnerTeam[1]);
 
-  it('should render the partner Mon équipe', () => {
-    const { getAllByText } = render(
-      <LoginContext.Provider
-        value={{
-          isLogged: true,
-          setIsLogged: () => undefined,
-        }}
-      >
-        <AccountContext.Provider
-          value={{ setAccountToDelete, accountToDelete, deleteAction }}
-        >
-          <PartnerHomePage />
-        </AccountContext.Provider>
-      </LoginContext.Provider>
-    );
-    const partnerStats = getAllByText('Mon équipe');
-    expect(partnerStats[0]).toBeInTheDocument();
-  });
-
-  it('should render the partner Mes établissements', () => {
-    const { getAllByText } = render(
-      <LoginContext.Provider
-        value={{
-          isLogged: true,
-          setIsLogged: () => undefined,
-        }}
-      >
-        <AccountContext.Provider
-          value={{ setAccountToDelete, accountToDelete, deleteAction }}
-        >
-          <PartnerHomePage />
-        </AccountContext.Provider>
-      </LoginContext.Provider>
-    );
-    const partnerStats = getAllByText('Mes établissements');
-    expect(partnerStats[0]).toBeInTheDocument();
-  });
-
-  it('should navigate to Accueil tab when button is cliked', () => {
-    // GIVEN
-    render(
-      <LoginContext.Provider
-        value={{
-          isLogged: true,
-          setIsLogged: () => undefined,
-        }}
-      >
-        <AccountContext.Provider
-          value={{ setAccountToDelete, accountToDelete, deleteAction }}
-        >
-          <PartnerHomePage />
-        </AccountContext.Provider>
-      </LoginContext.Provider>
-    );
-    // WHEN
-    const homeButton = screen.getAllByText('Accueil');
-    fireEvent.click(homeButton[1]);
-    // THEN
-    const tabTitle = screen.getByText(`Le petit mot de l'équipe C2S`);
-    const title = screen.getByText(/Ravi de vous retrouver/);
-
-    expect(tabTitle).toBeInTheDocument();
-    expect(title).toBeInTheDocument();
-  });
-
-  it('should render Mon équipe bloc', () => {
-    // Given
-    render(
-      <LoginContext.Provider
-        value={{
-          isLogged: true,
-          setIsLogged: () => undefined,
-        }}
-      >
-        <AccountContext.Provider
-          value={{ setAccountToDelete, accountToDelete, deleteAction }}
-        >
-          <PartnerHomePage />
-        </AccountContext.Provider>
-      </LoginContext.Provider>
-    );
-    // When
-    const homeButton = screen.getAllByText('Accueil');
-    fireEvent.click(homeButton[1]); // Accueil est present dans le fil d'ariane
-
-    // Then
-    expect(screen.getAllByText('Mon équipe').length).toEqual(2);
+      // THEN
+      await waitFor(() => {
+        expect(screen.getByText(/Les membres de votre équipe seront/)).toBeInTheDocument();
+      });
+    });
   });
 });

5b6f548da642c6a3ea4039ecdcf23ea59174262a
olivier-rabot <olivier.rabot@sg.social.gouv.fr>
2024-11-12 14:25:52 +0100

    feat: add test on moderators moderation page

---
+++ front/src/utils/tests/moderatorModerators.fixtures.ts
@@ -0,0 +1,21 @@
+export const ModeratorModeratorsAPIResponse = {
+  users: [
+    {
+      id: 10572,
+      nom: 'CLERC',
+      prenom: 'Michel',
+      email: 'michel.clerc@test.fr',
+      telephone: null,
+      fonction: 'Chargé de mission',
+    },
+    {
+      id: 10573,
+      nom: 'FELLER',
+      prenom: 'Louis',
+      email: 'louisfeller@test.fr',
+      telephone: '0763710085',
+      fonction: 'Chargé de mission',
+    },
+  ],
+  totalUsers: 2,
+};

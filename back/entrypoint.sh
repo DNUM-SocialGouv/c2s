3c219ff7fc417a4a47db4052c26c70113775c214
“SylvainBlancheDNUM” <sylvain.blanche@sg.social.gouv.fr>
2024-12-16 09:59:28 +0100

    fix: ajout des clés API Siren

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar /app/c2s-dev.jar --spring.config.location=file:/etc/c2s-dev/application.properties
+java -jar /app/c2s-dev.jar --spring.config.location=file:/etc/c2s-dev/application.properties -Dsiren.keyconsumer=${C2S_BACK_SIREN_KEY_CONSUMER} -Dsiren.secretconsumer=${C2S_BACK_SIREN_KEY_CONSUMER}

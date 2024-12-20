25fd854bd216f3fe64bef0280580ada803e85487
“SylvainBlancheDNUM” <sylvain.blanche@sg.social.gouv.fr>
2024-12-20 17:58:34 +0100

    fix: update statut sur compte OC de test et modérateurs

--- back/c2s-repository/src/main/resources/migrations/V1.31__update_membre_generique.sql
+++ back/c2s-repository/src/main/resources/migrations/V1.31__update_membre_generique.sql
@@ -1,1 +1,4 @@
-CREATE EXTENSION IF NOT EXISTS unaccent SCHEMA public;
+
+UPDATE membre SET statut='ACTIF' WHERE email='c2s_user_oc@c2s.com';
+
+UPDATE membre set statut='ACTIF' WHERE groupe='MODERATEUR';

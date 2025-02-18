ea2f765fe6f781c960d510db4a14640ec3f1f04e
olivier-rabot <167769451+olivier-rabot@users.noreply.github.com>
2025-02-18 11:19:28 +0100

    chore: comment function (#178)

--- front/src/utils/ModeratorEstablishments.helper.tsx
+++ front/src/utils/ModeratorEstablishments.helper.tsx
@@ -2,6 +2,13 @@
 import { QueryFilters } from '../components/moderatorEstablishments/establishments/Establishments.tsx';
 import { AddEstablishmentErrorResponseData } from '../domain/ModeratorEstablishments.ts';
 
+/**
+ * When a user types in a field, remove the field from the errors object to
+ * clear the error message. This is used in the AddEstablishmentForm to clear
+ * errors from the backend when the user types in a field.
+ * @param fieldNames The names of the fields to remove from the errors object
+ * @param setErrors The function to update the errors object
+ */
 export const handleInputChange = (
   fieldNames: string[],
   setErrors: React.Dispatch<

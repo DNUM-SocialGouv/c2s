22292f4cda0257d8d37a90fbcb3b3426c2cb2c04
sylvainblancheDNUM <sylvain.blanche@sg.social.gouv.fr>
2025-05-13 17:30:58 +0200

    fix: update insee config pour docker

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar /app/c2s-dev.jar --spring.config.location=file:/etc/c2s-dev/application.properties -Dsiren.api.key=${C2S_BACK_SIREN_API_KEY}
+java -jar /app/c2s-dev.jar --spring.config.location=file:/etc/c2s-dev/application.properties -Dsiren.api.key=${SIREN_API_KEY}

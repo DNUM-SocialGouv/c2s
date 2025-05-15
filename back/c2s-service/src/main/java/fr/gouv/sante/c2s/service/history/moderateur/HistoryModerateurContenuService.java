b8dfe058fbce2a67954f43f09b5f8804b8afc739
sylvainblancheDNUM <sylvain.blanche@sg.social.gouv.fr>
2025-05-15 09:56:31 +0200

    fix: logger inutilisé

--- back/c2s-service/src/main/java/fr/gouv/sante/c2s/service/history/moderateur/HistoryModerateurContenuService.java
+++ back/c2s-service/src/main/java/fr/gouv/sante/c2s/service/history/moderateur/HistoryModerateurContenuService.java
@@ -5,7 +5,6 @@ import fr.gouv.sante.c2s.model.SectionEnum;
 import fr.gouv.sante.c2s.model.dto.session.MembreSessionDTO;
 import fr.gouv.sante.c2s.model.entity.HistoricOperationEntity;
 import fr.gouv.sante.c2s.repository.HistoryOperationRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -13,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
-@Slf4j
 @Service
 @Transactional(propagation = Propagation.REQUIRES_NEW)
 public class HistoryModerateurContenuService {

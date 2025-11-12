f9607fdd7e63ad87bb6404bd9d12910b69e74c20
Youssouf DIALLO <youssouf.a.diallo@capgemini.com>
2025-11-12 16:04:31 +0100

    fix: version 1.0.53

--- back/c2s-model/src/main/java/fr/gouv/sante/c2s/model/C2SConstants.java
+++ back/c2s-model/src/main/java/fr/gouv/sante/c2s/model/C2SConstants.java
@@ -2,7 +2,7 @@ package fr.gouv.sante.c2s.model;
 
 public final class C2SConstants {
 
-    public static final String CURRENT_VERSION = "1.0.52";
+    public static final String CURRENT_VERSION = "1.0.53";
 
     private C2SConstants() {}
 

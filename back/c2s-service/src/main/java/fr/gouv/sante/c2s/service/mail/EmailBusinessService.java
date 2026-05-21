7c3eb740fd1c026139200dafe34cf9a14f2c5718
Youssouf DIALLO <youssouf.a.diallo@capgemini.com>
2026-05-21 17:05:32 +0200

    fix(C2S-414): ajouter la phrase manquantes dans le mail qui invite à initialiser le mot de passe

--- back/c2s-service/src/main/java/fr/gouv/sante/c2s/service/mail/EmailBusinessService.java
+++ back/c2s-service/src/main/java/fr/gouv/sante/c2s/service/mail/EmailBusinessService.java
@@ -50,7 +50,7 @@ public class EmailBusinessService {
                 +"Votre inscription à l'espace Partenaires de la C2S a été validée. Vous pouvez à présent définir votre mot de passe en suivant ce <a href='"+resetLink+"'>lien</a>.<br/><br/>"
                 +"<strong>NB</strong>: Veuillez noter que ce lien n'est valable que pendant 2 heures.<br/><br/>"
                 +"<br/>"
-                //+"Votre identifiant correspond à l'adresse mail utilisée lors de votre inscription.<br/><br/>"
+                +"Votre identifiant correspond à l'adresse mail utilisée lors de votre inscription.<br/><br/>"
                 +"A très vite au sein de votre espace !<br/><br/>"
                 +"<b>L'équipe C2S</b>"
                 +"</body>"
